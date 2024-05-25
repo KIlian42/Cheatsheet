d793e4b1dfbc7fda847426e5fff3dd4e9b056bbd
KIlian42 <57774167+KIlian42@users.noreply.github.com>
2024-05-25 16:40:15 +0200

    Adapt requirements.txt

--- PyTorch/pytorch_template.py
+++ PyTorch/pytorch_template.py
@@ -1,9 +1,17 @@
 # AutoGrad package -> gradient calculation, backpropagation
-
 import torch
 
 def main():
-    pass
+    x = torch.randn(3, requires_grad=True)
+
+    y = x+2
+    print(y)
+    z = y*y*2
+    z = z.mean()
+    print(z)
+
+    z.backward()
+    print(x.grad)
 
 if __name__ == "__main__":
     main()
